610648c690768f66d11b9b7cae45c96e2cbb57f2
Bassam Maged <bassammaged01@gmail.com>
2019-08-27 03:30:41 +0200

    Create extractSerial.py

---
+++ Forensics/USB Ripper/scripts/extractSerial.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.7
+def main():
+    
+    print('[+] Starting Extraction...\n')
+    fileToExtraction    = open('syslog.txt','r')
+    searchWords         = 'SerialNumber:'
+    serialResult  = open('serial.txt','a')
+    for line in fileToExtraction:
+            if (searchWords in line):
+                arr     = line.split('SerialNumber:')
+                serial   = arr[1]
+                serial   = serial.strip()
+                serial   = serial + '\r\n'
+                serialResult.write(serial)
+    print('End..')
+
+if __name__ == '__main__':
+    main()
