ad63b0b64745f019316fa47954c539af1a4f141c
Bassam Maged <bassammaged01@gmail.com>
2019-08-27 03:32:25 +0200

    Create serialCompare.py

---
+++ Forensics/USB Ripper/scripts/serialCompare.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3.7
+import os
+
+def main():
+    print('start Comparing...')
+    serialFile   = open('serial.txt','r')
+    serialFound  = open('serialFound.txt','a')
+    i           = 1
+    for line in serialFile:
+        line = line.rstrip()
+        cmd             = 'cat auth.json ' + '| ' + 'grep ' + '"' + line + '"'
+        resultCommand   = os.popen(cmd).read()
+        if (len(resultCommand) != 0):
+            result = 'Found!\r\n'
+            serialFound.write(result)
+        else:
+            result = 'Missing: ' + line + '\r\n'
+            serialFound.write(result)
+        result = result.strip()
+        print('[PROCESSING] {} , [RESULT]: ({})'.format(i,result))
+        i = i + 1
+if __name__ == "__main__":
+    main()
