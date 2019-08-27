dbcef79ceffe989f57c8b87ccd8e5a1070bd21f2
Bassam Maged <bassammaged01@gmail.com>
2019-08-27 03:31:05 +0200

    Create extractManuf.py

---
+++ Forensics/USB Ripper/scripts/extractManuf.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.7
+def main():
+    
+    
+    print('[+] Starting Extraction...\n')
+    fileToExtraction    = open('syslog.txt','r')
+    searchWords         = 'Manufacturer'
+    manufacturerResult  = open('manuf.txt','a')
+    for line in fileToExtraction:
+            if (searchWords in line):
+                arr     = line.split('Manufacturer:')
+                manuf   = arr[1]
+                manuf   = manuf.strip()
+                manuf   = manuf + '\r\n'
+                manufacturerResult.write(manuf)
+    print('End..')
+
+if __name__ == '__main__':
+    main()
